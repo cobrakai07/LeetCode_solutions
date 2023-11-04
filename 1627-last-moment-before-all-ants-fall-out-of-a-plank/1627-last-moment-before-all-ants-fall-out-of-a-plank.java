df4ca67cfc8d5355dce37640b8cbd830c62492f6
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2023-11-04 17:15:23 +0530

    Time: 0 ms (100%), Space: 43.6 MB (56.04%) - LeetHub

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,44 +1,29 @@
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        int time=0;
-
-        ArrayList<Character>r=new ArrayList<>();
-        ArrayList<Character>l=new ArrayList<>();
-
-        for(int i=0;i<=n;i++)
-        {
-            r.add('X');
-            l.add('X');
-        }
-
-        for(int i=0;i<left.length;i++)
-            l.set(left[i],'A');
-
-        for(int i=0;i<right.length;i++)
-            r.set(right[i],'A');
         
-
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<right.length;i++)
+        {
+            if(right[i]<min)
+            min=right[i];
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<left.length;i++)
+        {
+            if(left[i]>max)
+            max=left[i];
+        }
+        
         int rpos=-1,lpos=-1;
-        for(int i=0;i<r.size();i++)
-        {
-            if(r.get(i)=='A')
-            {
-                rpos=i;
-                break;
-            }
-        }
-        for(int i=l.size()-1;i>=0;i--)
-        {
-            if(l.get(i)=='A')
-            {
-                lpos=i;
-                break;
-            }
-
-        }
-        System.out.println(rpos);System.out.println(lpos);
+        if(min!=Integer.MAX_VALUE)
+            rpos=min;
+        
+        if(max!=Integer.MIN_VALUE)
+            lpos=max;
+        
         
         if(lpos==-1&&rpos==-1)return 0;
+
         int rtime=-1;
         boolean flag=false;
 
@@ -47,10 +32,9 @@ class Solution {
            rtime=n-rpos;
            flag=true;
        }
+       
         int ltime=lpos;
-       // if(lpos==-1)return ltime;
         if(rpos==-1&&flag)return rtime;
-
         if(rtime>ltime)return rtime;
         else return ltime;
     
