027316c157651348c8cf732b2a83dab426886d34
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-08-05 22:03:37 +0530

    Time: 89 ms (7.9%), Space: 60.7 MB (44.36%) - LeetHub

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/3225-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/3225-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,25 +1,20 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        Map<Integer,Integer>mp = new HashMap<>();
-        int s=0, e=0, max=0;
-        while(e<nums.length){
-            
-            if(mp.get(nums[e])==null || mp.get(nums[e])<=k){
-                mp.put( nums[e], mp.getOrDefault(nums[e],0)+1 );
-                if(mp.get(nums[e])<=k){max=Math.max(max,e-s+1);}
-                else{
-                    while(mp.get(nums[e])>k){
-                    mp.put( nums[s], mp.get(nums[s])-1 );
-                    s++;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int s = 0, e = 0, max = 0;
+        while (e < nums.length) {
+
+            if (mp.get(nums[e]) == null || mp.get(nums[e]) <= k) {
+                mp.put(nums[e], mp.getOrDefault(nums[e], 0) + 1);
+                if (mp.get(nums[e]) <= k) {
+                    max = Math.max(max, e - s + 1);
+                } else {
+                    while (mp.get(nums[e]) > k) {
+                        mp.put(nums[s], mp.get(nums[s]) - 1);
+                        s++;
+                    }
                 }
-                }
-                // System.out.println(mp+" "+s+","+e+" --"+max);
                 e++;
-            }else{
-                // while(mp.get(nums[e])>k){
-                //     mp.put( nums[s], mp.get(nums[s])-1 );
-                //     s++;
-                // }
             }
         }
         return max;
