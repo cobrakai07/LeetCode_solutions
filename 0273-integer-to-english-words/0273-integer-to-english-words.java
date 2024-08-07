72ffcd624acfcb77d0fa56ecd4c149dec713d717
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-08-07 10:25:11 +0530

    Time: 1 ms (100%), Space: 41.4 MB (96.58%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,39 +1,31 @@
 class Solution {
 
-    private static final String[] belowTen = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-    private static final String[] belowTwenty = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-    private static final String[] belowHundred = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-
+    private final String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
     public String numberToWords(int num) {
-
         if (num == 0) {
             return "Zero";
         }
-
-        return convertToWords(num);
+        return helper(num);
     }
 
-
-    private String convertToWords(int num) {
-        if (num < 10) {
-            return belowTen[num];
-        }
+    private String helper(int num) {
+        StringBuilder result = new StringBuilder();
         if (num < 20) {
-            return belowTwenty[num - 10];
+            result.append(belowTwenty[num]);
+        } else if (num < 100) {
+            result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+        } else if (num < 1000) {
+            result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+        } else if (num < 1000000) {
+            result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+        } else if (num < 1000000000) {
+            result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+        } else {
+            result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
         }
-        if (num < 100) {
-            return belowHundred[num / 10] + (num % 10 != 0 ? " " + convertToWords(num % 10) : "");
-        }
-        if (num < 1000) {
-            return convertToWords(num / 100) + " Hundred" + (num % 100 != 0 ? " " + convertToWords(num % 100) : "");
-        }
-        if (num < 1000000) {
-            return convertToWords(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + convertToWords(num % 1000) : "");
-        }
-        if (num < 1000000000) {
-            return convertToWords(num / 1000000) + " Million" + (num % 1000000 != 0 ? " " + convertToWords(num % 1000000) : "");
-        }
-        return convertToWords(num / 1000000000) + " Billion" + (num % 1000000000 != 0 ? " " + convertToWords(num % 1000000000) : "");
+        return result.toString().trim();
     }
 }
