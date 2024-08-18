201a4c2f73b84b5428dc69e318d5da887516249c
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-08-18 21:29:22 +0530

    Time: 0 ms (100%), Space: 41.4 MB (95.67%) - LeetHub

---
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+         int[]p1=new int [nums.length];
+        int[]p2=new int [nums.length];
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            p1[i]=sum;
+        }
+
+        sum=0;
+        for(int i=nums.length-1;i>=0;i--){
+            sum=sum+nums[i];
+            p2[i]=sum;
+        }
+
+        // System.out.println(Arrays.toString(p1));
+                // System.out.println(Arrays.toString(p2));
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                if(nums.length==1 || p2[1]==0)return 0;
+            }
+            else if(i==nums.length-1){
+                if(p1[nums.length-2]==0)return i;
+            }else{
+                if(p1[i-1]==p2[i+1])return i;
+            }
+        }
+        return -1;
+    }
+}
