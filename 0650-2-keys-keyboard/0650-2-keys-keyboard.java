cccd1bbce785329317713768c001692efad5dd0a
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-08-19 14:49:58 +0530

    Time: 74 ms (5.12%), Space: 55.9 MB (7.19%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fun(int copyContent, int curr, int num, int[][]dp){
+
+        if(curr>num)return 888888;
+        if(curr==num)return 0;
+        if(dp[copyContent][curr]!=Integer.MAX_VALUE)return dp[copyContent][curr];
+        int copyAndPaste=Integer.MAX_VALUE;
+        int paste=Integer.MAX_VALUE;
+
+        copyAndPaste=2+fun(curr,curr+curr,num,dp);
+        paste=1+fun(copyContent,curr+copyContent,num,dp);
+
+        return  dp[copyContent][curr]=Math.min( copyAndPaste, paste );
+    }
+    public int minSteps(int n) {
+        if(n==1)return 0;
+        int [][]dp= new int[1001][1001];
+        for(int []x: dp)Arrays.fill(x,Integer.MAX_VALUE);
+        return fun(1,1,n,dp)+1;
+    }
+}
