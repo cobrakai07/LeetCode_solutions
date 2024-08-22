4052e7154f107dce49f38f726d25c3c0bf3b5ca9
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-08-22 18:02:21 +0530

    Time: 1 ms (26.96%), Space: 40.6 MB (27.99%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findComplement(int num) {
+        // Step 1: Convert the number to binary and find its complement
+        StringBuilder sb = new StringBuilder();
+        while(num != 0){
+            int r = num % 2;
+            sb.append(r == 1 ? '0' : '1'); // Find complement (0->1, 1->0)
+            num = num / 2;
+        }
+        
+        // Step 2: Reverse the binary string as we built it in reverse order
+        sb.reverse();
+        
+        // Step 3: Convert the complemented binary string back to a decimal number
+        int ans = 0;
+        int length = sb.length();
+        for(int i = 0; i < length; i++){
+            if(sb.charAt(i) == '1') {
+                ans += (int)Math.pow(2, length - 1 - i);
+            }
+        }
+        
+        return ans;
+    }
+}
