f7e00979668e82a724cad8adc09ef365d8ae801f
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-08-28 19:13:29 +0530

    Time: 27 ms (47.71%), Space: 56 MB (54.48%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,46 +1,32 @@
 class Solution {
-    void merge(int arr[], int l, int m, int r)
-    {
-        int n1 = m - l + 1;
-        int n2 = r - m;
- 
-        int L[] = new int[n1];
-        int R[] = new int[n2];
 
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
- 
-        int i = 0, j = 0;
- 
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
- 
+    void merge(int[] arr, int left, int middle, int right){
 
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        int n1=middle-left+1;
+        int n2=right-middle;
+        int arr1[]=new int[n1];
+        int arr2[]= new int[n2];
+
+        for(int i=left,idx=0;i<=middle;i++,idx++)
+            arr1[idx]=arr[i];
+        for(int i=middle+1,idx=0;i<=right;i++,idx++)
+            arr2[idx]=arr[i];
+        
+        int k=left;
+        int p1=0,p2=0;
+        while(p1<arr1.length && p2<arr2.length){
+            if(arr1[p1]<arr2[p2])
+                arr[k++]=arr1[p1++];
+            else
+                arr[k++]=arr2[p2++];
         }
- 
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+
+        while(p1<arr1.length)
+            arr[k++]=arr1[p1++];
+        while(p2<arr2.length)
+            arr[k++]=arr2[p2++];
     }
-
+    
     void sort(int arr[], int l, int r)
     {
         if (l < r) 
