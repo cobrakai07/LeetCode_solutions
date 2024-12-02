fe6001b11d7153b9a9df6c97093c84dcf20714ff
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-12-02 14:33:48 +0530

    Time: 276 ms (100%), Space: 57.5 MB (100%) - LeetHub

---
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int getLargestOutlier(int[] nums) {
+        int sum=0;
+        int out=-1000;
+        Set<Integer>set= new HashSet<>();
+        for(int i: nums){
+            sum+=i;
+            set.add(i);
+        }
+        Map<Integer,Set<Integer>>mp= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(mp.get(nums[i])==null){
+                Set<Integer>set1= new HashSet<>();
+                set1.add(i);
+                mp.put(nums[i],set1);
+            }else{
+                Set<Integer>set1=mp.get(nums[i]);
+                set1.add(i);
+                mp.put(nums[i],set1);
+            } 
+        }
+        
+        for(int ii=0; ii<nums.length;ii++){
+            int i=nums[ii];
+            int diff=sum-(i*2);
+            if(mp.get(diff)!=null &&  diff+i+(sum-diff-i)==sum){
+                Set<Integer>set1=mp.get(diff);
+                if(set1.contains(ii)&&set1.size()==1)continue;
+                // System.out.println(STR."sum: \{sum}, diff: \{diff}, number: \{i}");
+                out=Math.max(out,diff);
+            }
+        }
+
+        return out;
+        
+    }
+}
