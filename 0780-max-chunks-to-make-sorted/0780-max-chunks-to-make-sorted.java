78683ed351c614ab2ffbefeabaff5000475d0e0a
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2024-12-19 22:39:11 +0530

    Time: 0 ms (100%), Space: 40.7 MB (85.6%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
@@ -2,27 +2,20 @@ class Solution {
 
     public int maxChunksToSorted(int[] arr) {
         int n = arr.length;
-        int[] prefixMax = arr.clone();
-        int[] suffixMin = arr.clone();
+        int chunks = 0, prefixSum = 0, sortedPrefixSum = 0;
 
-        // Fill the prefixMax array
-        for (int i = 1; i < n; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], prefixMax[i]);
-        }
-
-        // Fill the suffixMin array in reverse order
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(suffixMin[i + 1], suffixMin[i]);
-        }
-
-        int chunks = 0;
+        // Iterate over the array
         for (int i = 0; i < n; i++) {
-            // A new chunk can be created
-            if (i == 0 || suffixMin[i] > prefixMax[i - 1]) {
+            // Update prefix sum of `arr`
+            prefixSum += arr[i];
+            // Update prefix sum of the sorted array
+            sortedPrefixSum += i;
+
+            // If the two sums are equal, the two prefixes contain the same elements; a chunk can be formed
+            if (prefixSum == sortedPrefixSum) {
                 chunks++;
             }
         }
-
         return chunks;
     }
 }
