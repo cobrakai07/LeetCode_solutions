b9fd5e3855ba6e1dec7deaea524d5a0da33627ac
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-01-04 11:58:06 +0530

    Time: 0 ms (100%), Space: 45.2 MB (32.81%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,43 +1,25 @@
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-        int[]colored=new int[graph.length];
-        Queue<Integer>q=new ArrayDeque<>();
+    public boolean fun(int node, int[][]graph, int[]color){
 
-        for(int x=0;x<colored.length;x++)
-        {
-            if(colored[x]==1||colored[x]==2)continue;
-            //
-            q.offer(x);
-            colored[x]=1;
-        while(!q.isEmpty())
-        {
-            int idxAdjList=q.poll();
-            for(int i=0;i<graph[idxAdjList].length;i++)
-            {
-                if(colored[graph[idxAdjList][i]]==0)
-                {
-                    if(colored[idxAdjList]==1)
-                       colored[graph[idxAdjList][i]]=2;
-                    else
-                       colored[graph[idxAdjList][i]]=1;
-
-                    q.offer(graph[idxAdjList][i]);
-                }
-                else
-                {
-                    if(colored[idxAdjList]==colored[graph[idxAdjList][i]])
-                      return false;
-                }
+        for(int i=0;i<graph[node].length;i++){
+            if(color[ graph[node][i] ]==-1){
+                color[graph[node][i]] = color[node]==1 ? 2: 1;
+                if(!fun(graph[node][i],graph,color))return false;
+            }else{
+                if(color[graph[node][i]] == color[node])return false;
             }
         }
-            //
-        }
         
-
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int[]color= new int[graph.length];
+        Arrays.fill(color,-1);
+        for(int i=0;i<graph.length;i++){
+            if(color[i]==-1){
+                if(!fun(i,graph,color))return false;
+            }
+        }
         return true;
     }
 }
-
-
-//[[],[2,4,6],[1,4,8,9],[7,8],[1,2,8,9],[6,9],[1,5,7,8,9],[3,6,9],[2,3,4,6,9],[2,4,5,6,7,8]]...due to this case, traverse on whole colored array
-// As there can be more than one components in a graph independent of each other
