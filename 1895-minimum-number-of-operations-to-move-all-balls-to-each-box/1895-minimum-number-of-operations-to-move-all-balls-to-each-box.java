7c21a41f64ffea6277fa8da046026aa639353934
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-01-06 11:55:52 +0530

    Time: 2 ms (100%), Space: 44.6 MB (86.28%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,12 +1,28 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int [] m = new int[boxes.length()];
-        for(int i = 0; i < boxes.length(); i++){
-            for(int j = 0; j < boxes.length(); j++){
-                if(boxes.charAt(j) == '1' && i !=  j)
-                    m[i] += Math.abs(i-j);
+    //keep track of how many balls there are to the right.
+    //calculate base case for first index. 
+    //when we iterate right, if we encounter a 1, 
+        int rightSideCount = 0; 
+        int leftSideCount = 0; 
+        int currentVal = 0;
+        char[] box = boxes.toCharArray();
+        for(int i = box.length - 1; i>= 0; i--){
+            if(box[i] == '1'){
+                rightSideCount += 1;
+                currentVal += i;
             }
-        }   
-        return m;
+        }
+        int[] result = new int[box.length];
+        for(int i =0; i<box.length; i++){
+            result[i] = currentVal;
+            if(box[i] == '1'){
+                rightSideCount --; 
+                leftSideCount ++;
+            }
+            currentVal = currentVal - rightSideCount;
+            currentVal = currentVal + leftSideCount; 
+        }
+        return result;
     }
 }
