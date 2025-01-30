d8cbdb3c0d12a115cf20654d54a636862dd5241b
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-01-30 14:00:31 +0530

    Time: 1 ms (100%), Space: 42.6 MB (49.56%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,70 +1,39 @@
-class Pair{
-    int row;
-    int col;
-    int time;
-
-    Pair(int row,int col,int time)
-    {
-        this.row=row;
-        this.col=col;
-        this.time=time;
-    }
-}
 class Solution {
     public int orangesRotting(int[][] grid) {
-
-        int visited[][]=new int[grid.length][grid[0].length];
-        Queue<Pair>q=new ArrayDeque<>();
-
-        int freshOrangesBefore=0;
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[i].length;j++)
-            {
-                if(grid[i][j]==2)
-                {
-                    q.offer(new Pair(i,j,0));
-                    visited[i][j]=2;
-                }
-                else
-                {
-                    visited[i][j]=0;
-                }
-
-                if(grid[i][j]==1)
-                  freshOrangesBefore++;
-            }
-        }
-
-        int ansTime=0;
-        int freshOrangesAfter=0;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-
-        while(!q.isEmpty())
-        {
-            Pair ptemp=q.poll();
-            
-            for(int i=0;i<drow.length;i++)
-            {
-                int ncol=ptemp.col+drow[i];
-                int nrow=ptemp.row+dcol[i];
-                ansTime=Math.max(ansTime,ptemp.time);
-
-                if(ncol<grid[0].length&&ncol>=0&&nrow<grid.length&&nrow>=0 && visited[nrow][ncol]==0 &&grid[nrow][ncol]==1)
-                {
-                    q.offer(new Pair(nrow,ncol,ptemp.time+1));
-                    visited[nrow][ncol]=2;
-                    freshOrangesAfter++;
-
+        Queue<int[]>q= new ArrayDeque<>();
+        int freshCount=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0; j<grid[i].length; j++){
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j,0});
+                }else if(grid[i][j]==1){
+                    freshCount++;
                 }
             }
         }
 
-        if(freshOrangesAfter!=freshOrangesBefore)return -1;//no of rotten oranges after is equal to no of fresh oranges before
+        int []dirX={0,1,0,-1};
+        int []dirY={1,0,-1,0};
 
-        return ansTime;
+        int totalTime=0;
 
-        
+        while(!q.isEmpty()){
+            int[] arr = q.poll();
+            int x=arr[0], y=arr[1], ct= arr[2];
+            totalTime= Math.max(totalTime,ct);
+            for(int i=0; i<4;i++){
+                int nx= x+dirX[i];
+                int ny= y+dirY[i];
+                if(nx>=0 && nx<grid.length && ny>=0 && ny<grid[0].length){
+                    if(grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        q.offer(new int[]{nx,ny,ct+1});
+                        freshCount--;
+                    }
+                }
+            }
+        }
+
+        return freshCount==0?totalTime:-1;
     }
 }
