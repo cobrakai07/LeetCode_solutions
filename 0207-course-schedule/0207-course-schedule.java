8c9fff53c94c186797c30250051d80708f5cfe9d
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-02-12 21:58:45 +0530

    Time: 4 ms (90.39%), Space: 45.3 MB (69.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,31 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        List<List<Integer>>adj=new ArrayList<>();
-        int []indegree=new int[numCourses];
-        List<Integer>topo=new ArrayList<>();
-        Queue<Integer>q=new ArrayDeque<>();
-
-        for(int i=0;i<numCourses;i++)
-        {
-            List<Integer>list=new ArrayList<>();
-            adj.add(list);
+    public boolean dfs(int node , int[]vis,int[] pathVis,List<List<Integer>>adj){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(int i : adj.get(node)){
+            if(vis[i]==0 &&dfs(i,vis,pathVis,adj))
+                return true;
+            else if(pathVis[i]==1)
+                return true;
         }
-        for(int i=0;i<prerequisites.length;i++)
-        {
-            int []arr=prerequisites[i];
-            List<Integer>list=adj.get(arr[1]);
-            list.add(arr[0]);
-            indegree[arr[0]]++;
-        }
-        
-        for(int i=0;i<indegree.length;i++)
-        {
-            if(indegree[i]==0)
-               q.offer(i);
-        }
-
-        while(!q.isEmpty())
-        {
-            int node=q.poll();
-            topo.add(node);
-            
-            for (int it : adj.get(node))
-            {
-                indegree[it]--;
-
-                if (indegree[it] == 0)
-                    q.offer(it);
-            }
-        }
-        if(topo.size()== numCourses)
-        return true;
-        else
+        pathVis[node]=0;
         return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>adj= new ArrayList<>();
+        int []vis= new int[numCourses];
+        int []pathVis= new int[numCourses];
+        for(int i: vis)adj.add(new ArrayList<>());
+        for(int []i:prerequisites){
+            int from = i[0], to = i[1];
+            adj.get(to).add(from);
+        }
+        // ArrayDeque<Integer>stk = new ArrayDeque<>();
 
+        for(int i=0;i<vis.length;i++){
+            if(vis[i]==0 &&dfs(i,vis,pathVis,adj))
+                return false;
+        }
+        return true;
     }
 }
