18c211b7129bfd7e3320a06c86ee1abed5e640b3
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-02-12 22:09:45 +0530

    Time: 4 ms (91.49%), Space: 46.2 MB (27.58%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,53 +1,44 @@
-class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-           List<List<Integer>>adj=new ArrayList<>();
-        int []indegree=new int[numCourses];
-        List<Integer>topo=new ArrayList<>();
-        Queue<Integer>q=new ArrayDeque<>();
 
-        for(int i=0;i<numCourses;i++)
-        {
-            List<Integer>list=new ArrayList<>();
-            adj.add(list);
-        }
-        for(int i=0;i<prerequisites.length;i++)
-        {
-            int []arr=prerequisites[i];
-            List<Integer>list=adj.get(arr[1]);
-            list.add(arr[0]);
-            indegree[arr[0]]++;
+class Solution {
+    public boolean dfs(int node, int[] vis, int[] pathVis, List<List<Integer>> adj, Stack<Integer> stk) {
+        vis[node] = 1;
+        pathVis[node] = 1; 
+        
+        for (int i : adj.get(node)) {
+            if (pathVis[i] == 1) return false;
+            if (vis[i] == 0) {
+                if (!dfs(i, vis, pathVis, adj, stk)) return false;
+            }
         }
         
-        for(int i=0;i<indegree.length;i++)
-        {
-            if(indegree[i]==0)
-               q.offer(i);
+        pathVis[node] = 0; 
+        stk.push(node);
+        return true;
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+
+        for (int[] i : prerequisites) {
+            int from = i[0], to = i[1];
+            adj.get(to).add(from);
         }
 
-        while(!q.isEmpty())
-        {
-            int node=q.poll();
-            topo.add(node);
-            
-            for (int it : adj.get(node))
-            {
-                indegree[it]--;
+        int[] vis = new int[numCourses];
+        int[] pathVis = new int[numCourses];
+        Stack<Integer> stk = new Stack<>();
 
-                if (indegree[it] == 0)
-                    q.offer(it);
+        for (int i = 0; i < numCourses; i++) {
+            if (vis[i] == 0) {
+                if (!dfs(i, vis, pathVis, adj, stk)) return new int[] {};
             }
         }
-        if(topo.size()== numCourses)
-        {
-            int ans[]=new int[topo.size()];
-            for(int i=0;i<topo.size();i++)
-            {
-                ans[i]=topo.get(i);
-            }
-            return ans;
-        } 
 
-        int ans[]={};//empty array
-        return ans;
+        int[] ans = new int[numCourses];
+        int i = 0;
+        while (!stk.isEmpty()) ans[i++] = stk.pop();
+        
+        return ans; 
     }
 }
