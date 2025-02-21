2408b5150e14cf18decb3e9db4fb17c3b243f10d
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-02-21 23:57:16 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -11,20 +11,28 @@ class Solution {
         return dp[n]= fun(n-1)+fun(n-2);
     }
     public int climbStairs(int n) {
-        int[]dp=new int[n+1];
+        // int[]dp=new int[n+1];
         // Arrays.fill(dp,-1);
         // return funMemo(n,dp);
 
-        dp[0]=1;
+        // dp[0]=1;
 
-        for(int i=1; i<=n ;i++){
-            int one=0, two=0;
-            if(i-1>=0)one=dp[i-1];
-            if(i-2>=0)two=dp[i-2];
-            dp[i]=one+two;
-        }
+        // for(int i=1; i<=n ;i++){
+        //     int one=0, two=0;
+        //     if(i-1>=0)one=dp[i-1];
+        //     if(i-2>=0)two=dp[i-2];
+        //     dp[i]=one+two;
+        // }
 
-        return dp[n];
-        
+        // return dp[n];
+
+        int pre1=1, pre2=0;
+
+        for(int i=0; i<=n ;i++){
+            int t=pre2;
+            pre2=pre1+pre2;
+            pre1=t;
+        } 
+        return pre2;
     }
 }
