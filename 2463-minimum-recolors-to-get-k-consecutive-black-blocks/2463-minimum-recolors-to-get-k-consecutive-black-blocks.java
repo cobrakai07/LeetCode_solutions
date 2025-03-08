746f7046e3d8758ef3dec7200de2be765f907dd3
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-03-08 14:40:03 +0530

    Time: 1 ms (92.1%), Space: 42 MB (12.67%) - LeetHub

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+
+        if(k==1){
+            if(blocks.indexOf('B')==-1)return 1;
+            else return 0;
+        }
+
+        int indexOne=0;
+        int indexTwo=0;
+
+        int blackCount=0;
+        int whiteCount=0;
+
+        int minimumSwapsRequired=Integer.MAX_VALUE;
+
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(indexTwo)=='B')blackCount++;
+            else whiteCount++;
+            indexTwo++;
+        }
+        indexTwo--;
+
+        if(whiteCount==0)return 0;
+
+        minimumSwapsRequired = Math.min(whiteCount,minimumSwapsRequired);
+
+        for(; indexTwo<blocks.length() ;){
+            // System.out.println(STR."\{minimumSwapsRequired} bc:\{blackCount} wc:\{whiteCount}  i:\{indexOne} j:\{indexTwo}");
+            minimumSwapsRequired = Math.min(whiteCount,minimumSwapsRequired);
+
+            if(blocks.charAt(indexOne)=='W')whiteCount--;
+            else blackCount--;
+            indexOne++;
+            if(indexTwo+1<blocks.length()){
+                if(blocks.charAt(indexTwo+1)=='W')whiteCount++;
+                else blackCount++;
+            }
+            
+            indexTwo++;
+        } 
+
+        return minimumSwapsRequired;       
+    }
+}
