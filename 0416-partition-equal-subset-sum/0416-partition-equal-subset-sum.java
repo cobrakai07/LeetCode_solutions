4c2c389450e1f0c7c909015c310b1018686ec6fc
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-04-29 15:43:24 +0530

    Time: 109 ms (10.43%), Space: 75.1 MB (5.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,25 +1,23 @@
 class Solution {
-    public boolean fun(int i, int sum, int[] nums, Boolean[][] dp) {
-        if (sum == 0) return true;
-        if (i == nums.length) return false;
-        if (dp[i][sum] != null) return dp[i][sum];
-        
-        boolean pick = false;
-        if (sum - nums[i] >= 0)
-            pick = fun(i + 1, sum - nums[i], nums, dp);
-        boolean notPick = fun(i + 1, sum, nums, dp);
-        
-        return dp[i][sum] = pick || notPick;
-    }
+    public boolean fun(int i, int t, int[]arr,Boolean[][]dp){
+        if(i==arr.length){
+            if(t==0)return true;
+            else return false;
+        }
+        if(t==0)return true;
+        if(t<0)return false;
+        if(dp[i][t]!=null)return dp[i][t];
+        boolean pick = fun(i+1, t-arr[i], arr,dp);
+        boolean npick = fun(i+1, t, arr,dp);
 
+        return  dp[i][t]=pick||npick;
+    }
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int i : nums) sum += i;
-        if (sum % 2 == 1) return false;
-        
-        int target = sum / 2;
-        Boolean[][] dp = new Boolean[nums.length][target + 1];
-        
-        return fun(0, target, nums, dp);
+        int sum=0;
+        for(int i: nums)sum+=i;
+        if(sum%2==1)return false;
+
+        Boolean[][]dp= new Boolean[nums.length][sum+1];
+        return fun(0,sum/2,nums,dp);
     }
 }
