b7d96d631556cb338f604fb1f1bfdac4f4316172
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-05-02 15:43:08 +0530

    Time: 0 ms (100%), Space: 40.8 MB (34.08%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,13 +1,6 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int xor = start ^ goal;  // XOR to find the differing bits
-        int count = 0;
-        
-        while (xor != 0) {
-            count += xor & 1;   // Add 1 to count if the LSB is 1
-            xor >>= 1;          // Shift right to check the next bit
-        }
-        
-        return count;
+        int xor = start ^ goal;
+        return Integer.bitCount(xor);
     }
 }
