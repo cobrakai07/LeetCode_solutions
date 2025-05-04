40d1fe8c7c403eedc3979a9ba8012afe7d2d2774
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-05-04 10:24:06 +0530

    Time: 4 ms (92.92%), Space: 44.6 MB (37.38%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,21 @@
 class Solution {
-    int fun(int i, int t, int []nums){
+    int fun(int i, int t, int []nums, Integer[][]dp){
         if(i==nums.length){
            return t==0? 1: 0;
         }
-        int pick= fun(i+1,t-nums[i],nums);
-        int npick= fun(i+1,t,nums);
+        if(t<0)return 0;
+        if(dp[i][t]!=null)return dp[i][t];
+        int pick= fun(i+1,t-nums[i],nums,dp);
+        int npick= fun(i+1,t,nums,dp);
 
-        return pick+npick;
+        return dp[i][t]=pick+npick;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int s=0;
         for(int i: nums)s+=i;
         if(s-target<0 || ((s-target)&1)==1)return 0;
-        return fun(0,(s-target)/2,nums);
+        int size= target<0?s-target:s+1;
+        Integer[][]dp = new Integer[nums.length][size];
+        return fun(0,(s-target)/2,nums,dp);
     }
 }
