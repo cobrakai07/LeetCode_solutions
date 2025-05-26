5704acf1c6cf994e646a21d6766ae14aaaa814e1
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-05-26 07:33:13 +0530

    Time: 9 ms (96.43%), Space: 69.2 MB (5.71%) - LeetHub

--- 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.java
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.java
@@ -1,19 +1,30 @@
+import java.util.*;
+
 class Solution {
-    int lowestPrimeFactor(int x){
-        for(int i=2;i*i<=x;i++){
-            if(x%i==0) return i;
-        }
-        return x;
-    }
-    public int minOperations(int[] nums) {
-        int n=nums.length,min_operations=0;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]>nums[i+1]){
-                min_operations++;
-                nums[i]=lowestPrimeFactor(nums[i]);
+    static final int MAX = 1000000;  // adjust based on expected max nums[i]
+    static int[] spf = computeSPF();
+
+    // Precompute smallest prime factor (SPF) for every number up to MAX
+    static int[] computeSPF() {
+        int[] spf = new int[MAX + 1];
+        for (int i = 2; i <= MAX; i++) {
+            if (spf[i] == 0) {  // i is prime
+                for (int j = i; j <= MAX; j += i) {
+                    if (spf[j] == 0) spf[j] = i;
+                }
             }
-            if(nums[i]>nums[i+1]){
-                return -1;
+        }
+        return spf;
+    }
+
+    public int minOperations(int[] nums) {
+        int n = nums.length, min_operations = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            while (nums[i] > nums[i + 1]) {
+                int lpf = spf[nums[i]];
+                if (lpf == nums[i]) return -1;  // prime, can't reduce further
+                nums[i] = lpf;
+                min_operations++;
             }
         }
         return min_operations;
