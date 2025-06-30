8a2fe16b1a263fb90590f1d2ea7e7b9689cbf513
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-07-01 00:41:00 +0530

    Time: 97 ms (59.18%), Space: 82.7 MB (28.83%) - LeetHub

---
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -0,0 +1,127 @@
+class Dsu {
+    int[] parent;
+    int[] size;
+
+    Dsu(int s) {
+        size = new int[s];
+        parent = new int[s];
+
+        for (int i = 0; i < s; i++) {
+            size[i] = 1;
+            parent[i] = i;
+        }
+    }
+
+    public int find_ulp(int node) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = find_ulp(parent[node]);
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = find_ulp(u);
+        int ulp_v = find_ulp(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+}
+
+class Solution {
+    public int largestIsland(int[][] grid) {
+
+        Dsu dsu = new Dsu(grid.length * grid[0].length);
+
+        int[] dx = { 1, 0, -1, 0 };
+        int[] dy = { 0, 1, 0, -1 };
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int node = i * (grid[0].length) + j;
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx < 0 || ny < 0 || nx >= grid.length || ny >= grid[0].length)
+                            continue;
+                        if (grid[nx][ny] == 1) {
+                            int nodeAdj = nx * (grid[0].length) + ny;
+                            dsu.unionBySize(node, nodeAdj);
+                        }
+                    }
+                }
+            }
+        }
+
+        int maxArea = 0;
+
+        // for(int i=0; i<grid.length;i++){
+        //     for(int j=0;j<grid[0].length;j++){
+        //         if(grid[i][j]==0){
+        //             int node = i*(grid[0].length)+j;
+        //             for(int k = 0; k<4 ; k++){
+        //                 int nx = i+dx[k];
+        //                 int ny = j+dy[k];
+        //                 if(nx<0||ny<0||nx>=grid.length||ny>=grid[0].length)continue;
+        //                 Set<Integer>set= new HashSet<>();
+        //                 int currentCount=0;
+        //                 if(grid[nx][ny]==1){
+        //                     int nodeAdj = nx*(grid[0].length)+ny;
+        //                     int ulp_u = dsu.find_ulp(node);
+        //                     int ulp_v = dsu.find_ulp(nodeAdj);
+        //                     if(ulp_u!=ulp_v && !set.contains(ulp_v)){
+        //                         currentCount+= dsu.size[ulp_u]+dsu.size[ulp_v]+1;
+        //                         maxArea=Math.max(maxArea,currentCount);
+        //                     }
+        //                     // dsu.unionBySize(node,nodeAdj);
+        //                 }
+        //             }
+        //         }
+
+        //     }
+        // }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    Set<Integer> set = new HashSet<>();
+                    int currentArea = 1; // flip this 0 to 1
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx < 0 || ny < 0 || nx >= grid.length || ny >= grid[0].length)
+                            continue;
+                        if (grid[nx][ny] == 1) {
+                            int nodeAdj = nx * grid[0].length + ny;
+                            int ulp = dsu.find_ulp(nodeAdj);
+                            if (!set.contains(ulp)) {
+                                set.add(ulp);
+                                currentArea += dsu.size[ulp];
+                            }
+                        }
+                    }
+                    maxArea = Math.max(maxArea, currentArea);
+                }
+            }
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int node = i * grid[0].length + j;
+                    int ulp = dsu.find_ulp(node);
+                    maxArea = Math.max(maxArea, dsu.size[ulp]);
+                }
+            }
+        }
+
+        // System.out.println(Arrays.toString(dsu.parent));
+        return maxArea;
+
+    }
+}
