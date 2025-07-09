ff0e3d76dfd83a692d1d3a3c90eaa074556f0953
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-07-09 23:58:55 +0530

    Time: 1258 ms (7.2%), Space: 54.8 MB (5.04%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,50 +1,23 @@
-class Solution 
-{
-    public List<List<Integer>> threeSum(int[] nums) 
-    {
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        // Sort the array
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>>set = new HashSet<>();
+        HashMap<Integer,Integer>mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(mp.get(-nums[i]-nums[j])!=null){
+                    int idx = mp.get(-nums[i]-nums[j]);
+                    if(idx!=i && idx!=j){
+                        List<Integer>l=new ArrayList<>();
+                        l.add(nums[i]);l.add(nums[j]);l.add(nums[idx]);
+                        Collections.sort(l);
+                        set.add(l);
                     }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
                 }
             }
         }
-        return ans;
+        return new ArrayList<>(set);
     }
 }
