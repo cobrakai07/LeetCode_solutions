bc72d78d5550fcd10e3b1a19473a0f68454aa750
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-07-21 09:19:27 +0530

    Time: 56 ms (14.9%), Space: 55 MB (5.56%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,24 +1,27 @@
+record Pair(char ch, int c){}
 class Solution {
     public String makeFancyString(String s) {
-        StringBuilder sb = new StringBuilder();
-        int p1=0,p2=0;
-        int count=0;
-        while(p2<s.length()){
-            if(s.charAt(p1)==s.charAt(p2)){
-                if(count>=2){
-                    count++;
-                    p2++;
-                }else{
-                    count++;
-                    sb.append(s.charAt(p2));
-                    p2++;
-                }
+        ArrayDeque<Pair>stk = new ArrayDeque<>();
+        for(char c: s.toCharArray()){
+            // System.out.println(stk);
+            if(stk.isEmpty()){
+                Pair p = new Pair(c,1);
+                stk.push(p);
             }else{
-                count=0;
-                p1=p2;
+                Pair p = stk.peek();
+                if(p.ch()==c && p.c()==2){continue;}
+                else{
+                    if(p.ch()==c)
+                       { Pair p1 = new Pair(c,p.c()+1);
+                        stk.push(p1);}
+                    else
+                       { Pair p1 = new Pair(c,1);
+                        stk.push(p1); } 
+                }
             }
         }
-
-        return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        while(!stk.isEmpty())sb.append(stk.pop().ch());
+        return sb.reverse().toString();
     }
 }
