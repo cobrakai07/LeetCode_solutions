de6ee49e9a442bc92f87d81f9bf7285acda75d49
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-07-31 12:11:49 +0530

    Time: 3 ms (96.81%), Space: 60.2 MB (21.57%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[]sweep= new int[n];
+        int[]diff= new int [n];
         for(int i[]: bookings){
-            sweep[i[0]-1]+=i[2];
+            diff[i[0]-1]+=i[2];
             if(i[1]<n)
-                sweep[i[1]]-=i[2];
+                diff[i[1]]-=i[2];
         }
-        int prefix[]=new int[n];
-        prefix[0]=sweep[0];
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+sweep[i];
+            diff[i]=diff[i]+diff[i-1];
         }
-        // System.out.println(Arrays.toString(sweep));
-        return prefix;
+        return diff;
     }
 }
