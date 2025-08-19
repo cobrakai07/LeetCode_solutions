e01352f9725012e54a4cc792601914afec0156b7
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-08-20 00:00:08 +0530

    Time: 28 ms (89.71%), Space: 57.5 MB (64.07%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,42 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        ArrayDeque<Integer> st = new ArrayDeque<>();
+
+        int[] nse = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+            nse[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        int[] pse = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        int maxArea = 0;
+   
+        for (int i = 0; i < n; i++) {
+            int width = nse[i] - pse[i] - 1;
+            int currentArea = heights[i] * width;
+            maxArea = Math.max(maxArea, currentArea);
+        }
+
+        // System.out.println(Arrays.toString(nse));
+        // System.out.println(Arrays.toString(pse));
+
+        return maxArea;
+    }
+}
