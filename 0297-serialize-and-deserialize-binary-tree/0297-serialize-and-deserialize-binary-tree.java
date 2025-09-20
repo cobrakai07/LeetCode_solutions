141e3bef79cc969697f2a0141a1c29999dd19fec
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-09-20 12:00:05 +0530

    Time: 15 ms (48.93%), Space: 45.9 MB (58.27%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "#";
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                sb.append("null,");
+                continue;
+            }
+            sb.append(node.val).append(",");
+            q.offer(node.left);
+            q.offer(node.right);
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.equals("#")) return null;
+
+        String[] arr = data.split(",");
+        TreeNode root = new TreeNode(Integer.valueOf(arr[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int index = 1;
+
+        while (!q.isEmpty() && index < arr.length) {
+            TreeNode node = q.poll();
+
+            // left child
+            if (!arr[index].equals("null")) {
+                TreeNode left = new TreeNode(Integer.valueOf(arr[index]));
+                node.left = left;
+                q.offer(left);
+            }
+            index++;
+
+            // right child
+            if (index < arr.length && !arr[index].equals("null")) {
+                TreeNode right = new TreeNode(Integer.valueOf(arr[index]));
+                node.right = right;
+                q.offer(right);
+            }
+            index++;
+        }
+        return root;
+    }
+}
