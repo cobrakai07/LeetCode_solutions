a34252386f0f2da33f2697ed66af02f882b34d8a
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-09-23 22:28:02 +0530

    Time: 30 ms (70.83%), Space: 51.4 MB (83.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>>set = new HashSet<>();
-        HashMap<Integer,Integer>mp = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],i);
-        }
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(mp.get(-nums[i]-nums[j])!=null){
-                    int idx = mp.get(-nums[i]-nums[j]);
-                    if(idx!=i && idx!=j){
-                        List<Integer>l=new ArrayList<>();
-                        l.add(nums[i]);l.add(nums[j]);l.add(nums[idx]);
-                        Collections.sort(l);
-                        set.add(l);
-                    }
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        int k = n - 1;
+
+        while (i < n) {
+
+            j = i + 1;
+            k = n - 1;
+
+            while (j < k && j < n) {
+                if (nums[i] + nums[j] + nums[k] > 0)
+                    k--;
+                else if (nums[i] + nums[j] + nums[k] < 0)
+                    j++;
+                else {
+                    ans.add(List.of(nums[i], nums[j], nums[k]));
+                    int temp = nums[j];
+                    while (j < n && nums[j] == temp)
+                        j++;
+                    temp = nums[k];
+                    while (k >= 0 && nums[k] == temp)
+                        k--;
                 }
             }
+            int temp = nums[i];
+            while (i < n && nums[i] == temp)
+                i++;
         }
-        return new ArrayList<>(set);
+
+        return ans;
     }
 }
