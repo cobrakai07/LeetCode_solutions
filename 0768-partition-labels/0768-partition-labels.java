e7aa8392b3e7b2d7eb5188b75813cabf9dd57fc1
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-09-26 02:06:20 +0530

    Time: 5 ms (77.46%), Space: 42.5 MB (42.45%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,44 +1,66 @@
 class Solution {
-  public int[][] combineIntervals(int[][] intervals) {
-    Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-    int mergedCount = 0;
-    for (int i = 1; i < intervals.length; i++) {
-      if (intervals[i][0] <= intervals[mergedCount][1]) {
-        intervals[mergedCount][1] = Math.max(intervals[i][1], intervals[mergedCount][1]);
-      } else {
-        intervals[++mergedCount] = intervals[i];
-      }
+    public List<Integer> mergeIntervals(List<int[]> intervals) {
+
+        Collections.sort(intervals, (a, b) -> {
+            if (a[0] == b[0])
+                return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+
+        int start = intervals.get(0)[0];
+        int end = intervals.get(0)[1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            int in[] = intervals.get(i);
+
+            if (end >= in[0]) {
+                end = Math.max(end, in[1]);
+            } else {
+                mergedIntervals.add(new int[] { start, end });
+                end = in[1];
+                start = in[0];
+            }
+        }
+        mergedIntervals.add(new int[] { start, end });
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int[] in : mergedIntervals) {
+            ans.add(in[1] - in[0] + 1);
+        }
+
+        return ans;
     }
 
-    return Arrays.copyOf(intervals, mergedCount + 1);
-  }
+    public List<int[]> makeIntervals(String s) {
+        int[] startIdx = new int[26];
+        int[] endIdx = new int[26];
 
-  public List<Integer> partitionLabels(String s) {
+        Arrays.fill(startIdx, -1);
+        Arrays.fill(endIdx, -1);
 
-    Map<Character, int[]> mp = new HashMap<>();
-    List<Integer> ans = new ArrayList<>();
-    for (int i = 0; i < s.length(); i++) {
-      if (mp.get(s.charAt(i)) == null) {
-        int a[] = { i, i };
-        mp.put(s.charAt(i), a);
-      } else {
-        int a[] = mp.get(s.charAt(i));
-        a[1] = i;
-        mp.put(s.charAt(i), a);
-      }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (startIdx[c - 'a'] == -1)
+                startIdx[c - 'a'] = i;
+            endIdx[c - 'a'] = i;
+        }
+
+        List<int[]> intervals = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            if (startIdx[i] != -1)
+                intervals.add(new int[] { startIdx[i], endIdx[i] });
+        }
+
+        return intervals;
     }
 
-    int arr[][] = new int[mp.size()][2];
-    int idx = 0;
-    for (char c : mp.keySet())
-      arr[idx++] = mp.get(c);
-
-    // merge interval
-
-    arr = combineIntervals(arr);
-    for (int i[] : arr)
-      ans.add(i[1] - i[0] + 1);
-
-    return ans;
-  }
+    public List<Integer> partitionLabels(String s) {
+        List<int[]> intervals = makeIntervals(s);
+        return mergeIntervals(intervals);
+    }
 }
