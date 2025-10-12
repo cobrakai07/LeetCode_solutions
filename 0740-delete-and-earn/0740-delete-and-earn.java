b40125cf9a1b83eccb9e84b15ccb483e14a810cf
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-10-12 19:43:08 +0530

    Time: 15 ms (10.43%), Space: 45.7 MB (15.15%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        Arrays.sort(nums);
+        
+        // Step 1: aggregate same numbers
+        List<Integer> unique = new ArrayList<>();
+        List<Integer> points = new ArrayList<>();
+        int sum = nums[0], prev = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == prev) {
+                sum += nums[i];
+            } else {
+                unique.add(prev);
+                points.add(sum);
+                prev = nums[i];
+                sum = nums[i];
+            }
+        }
+
+        unique.add(prev);
+        points.add(sum);
+
+                System.out.println(unique);
+        System.out.println(points);
+        
+        // Step 2: memo array
+        int n = unique.size();
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        
+        // Step 3: recursive call
+        return fun(0, n, unique, points, dp);
+    }
+    
+    private int fun(int i, int n, List<Integer> unique, List<Integer> points, int[] dp) {
+        if (i >= n) return 0;
+        if (dp[i] != -1) return dp[i];
+        
+        // Option 1: skip current
+        int notPick = fun(i + 1, n, unique, points, dp);
+        
+        // Option 2: pick current
+        int next = i + 1;
+        if (next < n && unique.get(next) == unique.get(i) + 1) {
+            next++; // skip adjacent number
+        }
+        int pick = points.get(i) + fun(next, n, unique, points, dp);
+        
+        return dp[i] = Math.max(pick, notPick);
+    }
+}
