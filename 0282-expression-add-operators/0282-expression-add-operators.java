a40b4db78f9cad111a49cdb66c91ea175035f7f9
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-10-13 12:03:32 +0530

    Time: 108 ms (60.21%), Space: 45.8 MB (16.2%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+
+    public List<String> addOperators(String num, int target) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, "", num, target, 0, 0, 0);
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, String path, String num, int target,
+                           int index, long value, long prevNum) {
+
+        // base case
+        if (index == num.length()) {
+            if (value == target) ans.add(path);
+            return;
+        }
+
+        // build current number
+        for (int i = index; i < num.length(); i++) {
+            // prevent leading zeros
+            if (i != index && num.charAt(index) == '0') break;
+
+            String part = num.substring(index, i + 1);
+            long curr = Long.parseLong(part);
+
+            if (index == 0) {
+                // first number: start expression
+                backtrack(ans, part, num, target, i + 1, curr, curr);
+            } else {
+                // addition
+                backtrack(ans, path + "+" + part, num, target, i + 1, value + curr, curr);
+                // subtraction
+                backtrack(ans, path + "-" + part, num, target, i + 1, value - curr, -curr);
+                // multiplication
+                backtrack(ans, path + "*" + part, num, target, i + 1,
+                          value - prevNum + prevNum * curr, prevNum * curr);
+            }
+        }
+    }
+
+   
+}
