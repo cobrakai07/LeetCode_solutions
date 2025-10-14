3ff0cec040800534c7f9eaa877545946da0963da
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-10-14 10:07:51 +0530

    Time: 2 ms (47.79%), Space: 45.2 MB (7.35%) - LeetHub

---
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int[]inc= new int[nums.size()];
+        inc[0]=1;
+        for(int i=1;i<nums.size();i++){
+         if(nums.get(i-1)<nums.get(i)){
+            inc[i]=inc[i-1]+1;
+         }else{
+            inc[i]=1;
+         }
+        }
+
+        // System.out.println(Arrays.toString(inc));
+
+        if(k==1)
+            return  true;
+            
+        boolean occured =  false;
+        int prev = Integer.MIN_VALUE;
+
+        for(int i: inc){
+            if(i>=k*2)return true;
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=1;i<inc.length;i++){
+            if(inc[i-1]>=inc[i])
+                list.add(inc[i-1]);
+        }
+
+        if(inc[inc.length-1]>inc[inc.length-2])
+                list.add(inc[inc.length-1]);
+
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)>=k && list.get(i+1)>=k)return true;
+        }
+        // System.out.println(list);
+
+
+       
+        return false;
+    }
+}
