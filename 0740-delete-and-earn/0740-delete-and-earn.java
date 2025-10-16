d689782b040eadc6708ffa24e3d7bb7ef77efef3
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2025-10-16 12:15:54 +0530

    Time: 13 ms (21.12%), Space: 44.3 MB (84.92%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,52 +1,30 @@
 class Solution {
-    public int deleteAndEarn(int[] nums) {
-        Arrays.sort(nums);
+    public int fun(int i, int[]arr, Map<Integer,Integer>mp, int[]dp){
+        if(i>arr.length-1)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int pick = 0, npick = 0;
         
-        // Step 1: aggregate same numbers
-        List<Integer> unique = new ArrayList<>();
-        List<Integer> points = new ArrayList<>();
-        int sum = nums[0], prev = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == prev) {
-                sum += nums[i];
-            } else {
-                unique.add(prev);
-                points.add(sum);
-                prev = nums[i];
-                sum = nums[i];
-            }
-        }
-
-        unique.add(prev);
-        points.add(sum);
-
-                // System.out.println(unique);
-        // System.out.println(points);
-        
-        // Step 2: memo array
-        int n = unique.size();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        
-        // Step 3: recursive call
-        return fun(0, n, unique, points, dp);
+        int nextIdx = i+1 < arr.length ?( arr[i]+1 == arr[i+1]? i+2 : i+1 ) : i+1;
+        pick = arr[i]*mp.get(arr[i]) + fun(nextIdx,arr,mp,dp);
+        npick = fun(i+1, arr, mp,dp);
+        return dp[i] = Math.max(pick,npick);
     }
-    
-    private int fun(int i, int n, List<Integer> unique, List<Integer> points, int[] dp) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-        
-        // Option 1: skip current
-        int notPick = fun(i + 1, n, unique, points, dp);
-        
-        // Option 2: pick current
-        int next = i + 1;
-        if (next < n && unique.get(next) == unique.get(i) + 1) {
-            next++; // skip adjacent number
+    public int deleteAndEarn(int[] nums) {
+        Map<Integer,Integer>mp = new HashMap<>();
+        int size=0;
+        for(int i: nums){
+            if(mp.get(i)==null)size++;
+            mp.put(i, mp.getOrDefault(i,0)+1);
         }
-        int pick = points.get(i) + fun(next, n, unique, points, dp);
-        
-        return dp[i] = Math.max(pick, notPick);
+        int[]arr = new int[size];
+        int idx=0;
+        for(int k: mp.keySet()){
+            arr[idx++]=k;
+        }
+        Arrays.sort(arr);
+        int[]dp= new int[arr.length];
+        Arrays.fill(dp,-1);
+        return fun(0,arr,mp,dp);
+
     }
 }
