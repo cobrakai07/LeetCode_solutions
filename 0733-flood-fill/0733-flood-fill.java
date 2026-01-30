2ac4f61e39b23694fd05967a4898c25491ec6829
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-01-30 21:24:39 +0530

    Time: 1 ms (44.96%), Space: 47.4 MB (8.29%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,22 +1,21 @@
 class Solution {
-    public void dfs(int idx,int idy,int [][]arr,int c,int s){
-        
-        if(arr[idx][idy]==c)return;
-
-        arr[idx][idy]=c;
-
-        if((idx+1<arr.length)&&(arr[idx+1][idy]==s))//down
-            dfs(idx+1,idy,arr,c,s);
-        if((idx-1>=0&&idy>=0)&&(arr[idx-1][idy]==s))//up
-            dfs(idx-1,idy,arr,c,s);
-        if((idy+1<arr[idx].length)&&(arr[idx][idy+1]==s))//right
-            dfs(idx,idy+1,arr,c,s);
-        if((idy-1>=0)&&(arr[idx][idy-1]==s))//left
-            dfs(idx,idy-1,arr,c,s);
-
+    int dirX[] = {0,0,1,-1};
+    int dirY[] = {1,-1,0,0};
+    public void dfs(int x, int y, int sr, int sc, int color,int initColor, int[][]arr, int [][]vis){
+        if(vis[x][y]==1)return;
+        vis[x][y]=1;
+        arr[x][y] = color;
+        for(int i=0;i<4;i++){
+            int nx = x+dirX[i];
+            int ny = y+dirY[i];
+            if(nx>=0 && ny>=0 && nx < arr.length && ny< arr[0].length &&arr[nx][ny] ==initColor){
+                   dfs(nx,ny,sr,sc, color,initColor, arr,vis); 
+            }
+        }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        dfs(sr,sc,image,color,image[sr][sc]);
+        int [][]vis = new int[image.length][image[0].length];
+        dfs(sr,sc,sr,sc,color,image[sr][sc],image,vis);
         return image;
     }
 }
