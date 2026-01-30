64a27af022047bde4ab99b97242dfb125709e8c6
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-01-30 22:47:46 +0530

    Time: 2 ms (82.57%), Space: 47.9 MB (31.83%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,104 +1,35 @@
 class Solution {
+    int[] dirX = { 0, 0, -1, 1 };
+    int[] dirY = { 1, -1, 0, 0 };
 
-    public boolean isCoveredWithX(HashSet<Pair<Integer,Integer>>set,char[][]grid)
-    {
-        for(int i=0;i<grid[0].length;i++)
-        {
-            Pair<Integer,Integer>temp=new Pair(0,i);
-            if(set.contains(temp))return false;
-        }
-
-         for(int i=0;i<grid.length;i++)
-        {
-            Pair<Integer,Integer>temp=new Pair(i,0);
-            if(set.contains(temp))return false;
-        }
-
-        for(int i=0;i<grid[0].length;i++)
-        {
-            Pair<Integer,Integer>temp=new Pair(grid.length-1,i);
-            if(set.contains(temp))return false;
-        }
-
-         for(int i=0;i<grid.length;i++)
-        {
-            Pair<Integer,Integer>temp=new Pair(i,grid[0].length-1);
-            if(set.contains(temp))return false;
-        }
-
-        return true;
-    }
-
-    public void fillRegion(HashSet<Pair<Integer,Integer>>set,char[][]grid)
-    {
-        Iterator<Pair<Integer,Integer>> it = set.iterator();
-
-        while (it.hasNext()) 
-        {
-        Pair<Integer,Integer> idx = it.next();
-        grid[idx.getKey()][idx.getValue()]='X';
+    public void dfs(int x, int y, boolean [][]safe,  char[][]arr){
+        if(safe[x][y])return;
+        safe[x][y]=true;
+        
+        for (int i = 0; i < 4; i++) {
+                int nx = x + dirX[i];
+                int ny = y + dirY[i];
+                if (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length &&arr[nx][ny]=='O') {
+                    dfs(nx,ny,safe,arr);
+            }
         }
     }
-
-    public void infectedRegionCounter(int idx,int idy,int[][]visited,char [][]grid,HashSet<Pair<Integer,Integer>>set)//
-    {
-        if(visited[idx][idy]==1)return;
-
-        visited[idx][idy]=1;
-        set.add(new Pair(idx,idy));
-
-        if(idx-1>=0&&grid[idx-1][idy]=='O')
-        {
-            infectedRegionCounter(idx-1,idy,visited,grid,set);
-        }
-        if(idx+1<grid.length&&grid[idx+1][idy]=='O')
-        {
-            infectedRegionCounter(idx+1,idy,visited,grid,set);
-        }
-        if(idy-1>=0&&grid[idx][idy-1]=='O')
-        {
-            infectedRegionCounter(idx,idy-1,visited,grid,set);
-        }
-        if(idy+1<grid[0].length&&grid[idx][idy+1]=='O')
-        {
-            infectedRegionCounter(idx,idy+1,visited,grid,set);
-        }
-
-    }
-
     public void solve(char[][] board) {
-        int[][]visited=new int [board.length][board[0].length];
-        HashMap<HashSet<Pair<Integer,Integer>>,Integer>setOfRegions=new HashMap<>();
-
-        for(int i=0;i<board.length;i++)
-        {
-            for(int j=0;j<board[0].length;j++)
-            {
-                if(board[i][j]=='X')
-                {
-                    visited[i][j]=1;
-                }
-            }
+        boolean [][]safe = new boolean[board.length][board[0].length];
+        for(int i1=0,i2= board.length-1,j=0;j<board[0].length;j++){
+            if(board[i1][j]=='O')dfs(i1,j,safe,board);
+            if(board[i2][j]=='O')dfs(i2,j,safe,board);
         }
-        for(int i=0;i<board.length;i++)
-        {
-            for(int j=0;j<board[0].length;j++)
-            {
-                if(board[i][j]=='O')
-                {
-                    HashSet<Pair<Integer,Integer>>set=new HashSet<>();
-                    infectedRegionCounter(i,j,visited,board,set);
-                    if(set.size()!=0)
-                    setOfRegions.put(set,1);
-                }
-            }
+        for(int i1=0,i2= board[0].length-1,j=0;j<board.length;j++){
+            if(board[j][i1]=='O')dfs(j,i1,safe,board);
+            if(board[j][i2]=='O')dfs(j,i2,safe,board);
         }
 
-
-       for(HashSet<Pair<Integer,Integer>>key: setOfRegions.keySet() )
-       {
-           if(isCoveredWithX(key,board))
-             fillRegion(key,board);
-       }
+        for(int i=0;i<board.length;i++){
+            for(int j=0; j<board[0].length;j++){
+                if(!safe[i][j])
+                    board[i][j]='X';
+            }
+        }
     }
 }
