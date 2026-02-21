e9d895af456510aa28ebd9781a53005b12514cd3
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-02-21 12:48:10 +0530

    Time: 2 ms (98.83%), Space: 42.6 MB (16.23%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -0,0 +1,40 @@
+class Solution {
+    static int MAX = 65;     
+    static boolean[] isPrime;        
+
+    static {
+        isPrime = buildSieve(MAX); 
+    }
+
+    public static boolean[] buildSieve(int n) {
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+
+        if (n > 0) prime[0] = false;
+        if (n > 1) prime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        return prime;
+    }
+   
+    
+    public int countPrimeSetBits(int left, int right) {
+
+        int ans = 0;
+
+        while(left<=right){
+            int setBits = Integer.bitCount(left);
+            if(isPrime[setBits])ans++;
+            left++;
+        }
+
+        return ans;
+
+    }
+}
