8a740e563cd0dfe640303445a5f25da35fcf530e
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-03-14 14:09:16 +0530

    Time: 5 ms (53.94%), Space: 47.7 MB (6.23%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,44 +1,47 @@
 class Solution {
     String str="";
     int counter;
-    public void madeString(String s, int n, int k){
+    public void madeString(String s, int n, int k, boolean[]isFound){
+        if(isFound[0])return;
         if(n==0){
             counter++;
-            if(counter==k)
-              str = s;
+            if(counter==k){
+                str = s;
+                isFound[0] = true;
+            }
             return; 
         }
         if(s.length()==0){
             s = s+"a";
-            madeString(s,n-1,k);
+            madeString(s,n-1,k,isFound);
             s = s.substring(0,s.length()-1);
 
             s = s+"b";
-            madeString(s,n-1,k);
+            madeString(s,n-1,k,isFound);
             s = s.substring(0,s.length()-1);
 
             s = s+"c";
-            madeString(s,n-1,k);
+            madeString(s,n-1,k,isFound);
             s = s.substring(0,s.length()-1);
         }else{
             if(s.charAt(s.length()-1)!='a'){
             s = s+"a";
-            madeString(s,n-1, k);
+            madeString(s,n-1, k,isFound);
             s = s.substring(0,s.length()-1);}
 
             if(s.charAt(s.length()-1)!='b'){
             s = s+"b";
-            madeString(s,n-1, k);
+            madeString(s,n-1, k,isFound);
             s = s.substring(0,s.length()-1);}
 
             if(s.charAt(s.length()-1)!='c'){
             s = s+"c";
-            madeString(s,n-1, k);
+            madeString(s,n-1, k,isFound);
             s = s.substring(0,s.length()-1);}
         }
     }
     public String getHappyString(int n, int k) {
-        madeString("",n,k);
+        madeString("",n,k, new boolean[]{false});
         return str;
     }
 }
