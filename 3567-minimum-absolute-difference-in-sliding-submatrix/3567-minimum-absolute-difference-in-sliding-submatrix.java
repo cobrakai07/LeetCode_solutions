e5242bfb676231b4e18162614a94dc6361d08856
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-03-21 01:57:20 +0530

    Time: 37 ms (22.92%), Space: 46.9 MB (100%) - LeetHub

---
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findMin(int[][] arr, int i, int j, int k){
+        int min = Integer.MAX_VALUE;
+        Set<Integer> set = new HashSet<>();
+        for(int x = i;x<i+k && x<arr.length;x++){
+            for(int y=j;y<j+k && y<arr[0].length;y++){
+                set.add(arr[x][y]);
+            }
+        }
+        List<Integer>list = new ArrayList<>(set);
+        if(list.size()==1)return 0;
+        Collections.sort(list);
+        // System.out.println(list);
+        for(int it=0;it<list.size()-1;it++){
+            min = Math.min(min, Math.abs(list.get(it)- list.get(it+1)));
+            min = Math.min(min, Math.abs(list.get(it+1)- list.get(it)));
+        }
+        return min;
+    }
+    public int[][] minAbsDiff(int[][] grid, int k) {
+        int row = grid.length-k+1;
+        int col = grid[0].length-k+1;
+
+        int[][] ans = new int[row][col];
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                ans[i][j] = findMin(grid,i,j,k);
+            }
+        }
+
+        return ans;
+
+    }
+}
