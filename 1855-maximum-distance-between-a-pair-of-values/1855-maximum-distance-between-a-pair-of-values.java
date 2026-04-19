b62c873b38e2ada14eebbe9abfa3eda68a4c75df
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-04-19 17:42:56 +0530

    Time: 21 ms (11.19%), Space: 90.2 MB (21.36%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
@@ -1,32 +1,28 @@
 class Solution {
-    public void print(String s){
-         System.out.println(s);
-    }
-    public int binarySearch(int e, int val, int [] arr){
+
+    public int binarySearch(int e, int val, int[] arr) {
         int s = 0;
         int idx = -1;
         int storeE = e;
-        e = Math.min(e,arr.length-1);
-        // print("====");
-      
-        // print(s+","+e);
-        while(s<=e){
-            int m = s+(e-s)/2;
-            if(arr[m]<=val){
-                e = m -1;
+        e = Math.min(e, arr.length - 1);
+
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (arr[m] <= val) {
+                e = m - 1;
                 idx = m;
-            }else{
-                s = m+1;
+            } else {
+                s = m + 1;
             }
         }
-        
-        // print(idx+"---"+(storeE-idx));
-        return idx==-1?0:storeE-idx;
+
+        return idx == -1 ? 0 : storeE - idx;
     }
+
     public int maxDistance(int[] nums1, int[] nums2) {
         int maxDistance = 0;
-        for(int j=0;j<nums2.length;j++){
-            maxDistance = Math.max(maxDistance, binarySearch(j,nums2[j],nums1));
+        for (int j = 0; j < nums2.length; j++) {
+            maxDistance = Math.max(maxDistance, binarySearch(j, nums2[j], nums1));
         }
         return maxDistance;
     }
