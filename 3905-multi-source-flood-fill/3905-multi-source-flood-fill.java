4778dea8159597a389e7fe8fb1a3b90af05fcdfa
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-04-20 12:04:28 +0530

    Time: 51 ms (100%), Space: 92.4 MB (100%) - LeetHub

---
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int[][] colorGrid(int n, int m, int[][] sources) {
+
+        int[][] grid = new int[n][m];
+        int[][] time = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(time[i], Integer.MAX_VALUE);
+        }
+
+        Queue<int[]> q = new ArrayDeque<>();
+
+     
+        for (int[] s : sources) {
+            int x = s[0], y = s[1], color = s[2];
+            q.offer(new int[]{x, y});
+            grid[x][y] = color;
+            time[x][y] = 0;
+        }
+
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int x = curr[0], y = curr[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+
+              
+                if (time[nx][ny] > time[x][y] + 1) {
+                    time[nx][ny] = time[x][y] + 1;
+                    grid[nx][ny] = grid[x][y];
+                    q.offer(new int[]{nx, ny});
+                }
+                else if (time[nx][ny] == time[x][y] + 1) {
+                    grid[nx][ny] = Math.max(grid[nx][ny], grid[x][y]);
+                }
+            }
+        }
+
+        return grid;
+    }
+}
