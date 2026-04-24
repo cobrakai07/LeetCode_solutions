7693faf3c959ef6c01543ab5877dcde567d19faf
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-04-24 12:19:10 +0530

    Time: 608 ms (0.57%), Space: 271.8 MB (10%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
@@ -1,18 +1,25 @@
 class Solution {
-
+    public int dis(int x, int y, int i, char[] arr,int[][][] dp){
+        if(i==arr.length)return (int)Math.sqrt((x*x)+(y*y));
+        if(dp[x+50][y+50][i]!=-1)return dp[x+50][y+50][i];
+        char c = arr[i];
+        int maxdis = 0;
+        if(c=='L'){
+            maxdis = dis(x,y-1,i+1,arr,dp);
+        }else if(c=='R'){
+            maxdis = dis(x,y+1,i+1,arr,dp);
+        }else{
+            maxdis = Math.max(dis(x,y-1,i+1,arr,dp),dis(x,y+1,i+1,arr,dp));
+        }
+        return dp[x+50][y+50][i] = maxdis;
+    }
     public int furthestDistanceFromOrigin(String moves) {
-        int L = 0;
-        int R = 0;
-        int B = 0;
-        for (char c : moves.toCharArray()) {
-            if (c == 'L') {
-                L++;
-            } else if (c == 'R') {
-                R++;
-            } else {
-                B++;
+        int[][][] dp = new int[100][100][50];
+        for(int [][]a: dp){
+            for(int []b: a){
+                Arrays.fill(b,-1);
             }
         }
-        return Math.abs(L - R) + B;
+        return dis(0,0,0,moves.toCharArray(),dp);
     }
 }
