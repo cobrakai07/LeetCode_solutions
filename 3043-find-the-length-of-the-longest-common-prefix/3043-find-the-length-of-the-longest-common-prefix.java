98ddc4ec547709447954a1c6194d88192451493c
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-05-21 08:51:41 +0530

    Time: 39 ms (95.63%), Space: 77.9 MB (92.81%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -0,0 +1,52 @@
+class Node{
+    Node []arr;
+    Node(){
+        arr = new Node [10];
+    }
+}
+class Trie{
+    Node root;
+    Trie(){
+        root = new Node();
+    }
+
+    void add(int i,Node node, String num){
+        if(i==num.length()){
+            return ;
+        }
+        int number = num.charAt(i)-'0';
+        if(node.arr[number]==null){
+            node.arr[number] = new Node();
+            add(i+1,node.arr[number],num);
+        }else{
+            add(i+1,node.arr[number],num);
+        }
+    }
+
+    int prefixLen(int i,Node node,String num){
+        if(i==num.length())return 0;
+        int number = num.charAt(i)-'0';
+        if(node.arr[number]==null)return 0;
+        int len = 0;
+        len = 1 + prefixLen(i+1,node.arr[number],num);
+        return len;  
+    }
+}
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        Trie trie = new Trie();
+        for(int i: arr1){
+            String num = i+"";
+            trie.add(0,trie.root,num);
+        }
+
+        int max = 0;
+        
+        for(int i: arr2){
+            String num = i+"";
+            max = Math.max(max,trie.prefixLen(0,trie.root,num));
+        }
+
+        return max;
+    }
+}
