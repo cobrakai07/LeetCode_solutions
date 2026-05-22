31b048f09b28973b332a9809c4e1a383bf73ed2e
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-05-22 16:28:48 +0530

    Time: 7 ms (95.51%), Space: 43.4 MB (80.84%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,28 +1,85 @@
 class Solution {
-    public int repeatedStringMatch(String a, String b) {
-        if(a.length()>b.length()){
-            if(a.contains(b))return 1;
-            String str=a+a;
-            if(str.contains(b))return 2;
+
+    int rabinKarp(String str, String pattern) {
+
+        int n = str.length();
+        int m = pattern.length();
+
+        if (m > n)
             return -1;
-        }else if(a.length()==b.length()){
-            String str=a;
-            if(str.contains(b))return 1;
-            str=str+a;
-            if(str.contains(b))return 2;
-            return -1;
+
+        long base = 31;
+        long mod = 1_000_000_007L;
+
+        long power = 1;
+        long patternHash = 0;
+        long windowHash = 0;
+
+        // BASE^(m-1)
+        for (int i = 0; i < m - 1; i++) {
+            power = (power * base) % mod;
         }
 
-        String em="";
-        int ans=0;
-        while(em.length()<=b.length()+4){
-            em=a+em;
-            ans++;
-             if(em.contains(b))return ans;
+        for (int i = 0; i < m; i++) {
+
+            patternHash = ((patternHash * base)
+                    + (pattern.charAt(i) - 'a' + 1))
+                    % mod;
+
+            windowHash = ((windowHash * base)
+                    + (str.charAt(i) - 'a' + 1))
+                    % mod;
         }
-        if(em.contains(b))return ans;
+
+        for (int i = 0; i <= n - m; i++) {
+
+            if (windowHash == patternHash) {
+
+                if (str.substring(i, i + m)
+                        .equals(pattern)) {
+                    return i;
+                }
+            }
+
+            if (i < n - m) {
+
+                long leftChar = str.charAt(i) - 'a' + 1;
+
+                long newChar = str.charAt(i + m) - 'a' + 1;
+
+                windowHash = (windowHash
+                        - (leftChar * power) % mod
+                        + mod) % mod;
+
+                windowHash = ((windowHash * base)
+                        + newChar) % mod;
+            }
+        }
+
         return -1;
+    }
 
+    public int repeatedStringMatch(String a, String b) {
 
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        while (sb.length() < b.length()) {
+            sb.append(a);
+            count++;
+        }
+
+        if (rabinKarp(sb.toString(), b) != -1) {
+            return count;
+        }
+
+     
+        sb.append(a);
+
+        if (rabinKarp(sb.toString(), b) != -1) {
+            return count + 1;
+        }
+
+        return -1;
     }
 }
