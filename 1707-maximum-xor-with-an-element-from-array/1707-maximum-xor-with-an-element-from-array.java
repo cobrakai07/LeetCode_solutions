196fedc13c91293938b9f0e4f265eac1748b9be6
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-06-15 23:19:08 +0530

    Time: 152 ms (79.57%), Space: 225.2 MB (81.95%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
@@ -1,10 +1,8 @@
 class Node {
     Node[] arr;
-    boolean isLeaf;
 
     Node() {
         arr = new Node[2];
-        isLeaf = false;
     }
 }
 
@@ -15,98 +13,78 @@ class Trie {
         root = new Node();
     }
 
-    void insert(String s) {
+    void insert(int num) {
         Node temp = root;
-        for (int i = 0; i < s.length(); i++) {
 
-            int idx = s.charAt(i) - '0';
-            if (temp.arr[idx] == null) {
-                temp.arr[idx] = new Node();
+        for (int bit = 31; bit >= 0; bit--) {
+
+            int currBit = (num >> bit) & 1;
+
+            if (temp.arr[currBit] == null) {
+                temp.arr[currBit] = new Node();
             }
-            if (i == s.length() - 1)
-                temp.arr[idx].isLeaf = true;
-            temp = temp.arr[idx];
+
+            temp = temp.arr[currBit];
         }
     }
 
-    int max(String s) {
+    int maxXor(int num) {
         Node temp = root;
         int max = 0;
-        for (int i = 0, power = 31; i < s.length(); i++, power--) {
 
-            int idx = s.charAt(i) - '0';
-            if (idx == 1) {
-                if (temp.arr[0] != null) {
-                    max += (int) Math.pow(2, power);
-                    temp = temp.arr[0];
-                } else {
-                    temp = temp.arr[1];
-                }
+        for (int bit = 31; bit >= 0; bit--) {
+
+            int currBit = (num >> bit) & 1;
+            int opposite = 1 - currBit;
+
+            if (temp.arr[opposite] != null) {
+                max |= (1 << bit);
+                temp = temp.arr[opposite];
             } else {
-                if (temp.arr[1] != null) {
-                    max += (int) Math.pow(2, power);
-                    temp = temp.arr[1];
-                } else {
-                    temp = temp.arr[0];
-                }
+                temp = temp.arr[currBit];
             }
-
         }
+
         return max;
     }
 }
 
 class Solution {
-    public String pad(String s) {
-        if (s.length() < 32) {
-            int len = 32 - s.length();
-            StringBuilder sb = new StringBuilder();
-            while (len != 0) {
-                sb.append('0');
-                len--;
-            }
-
-            sb.append(s);
-            s = sb.toString();
-        }
-        return s;
-    }
 
     public int[] maximizeXor(int[] nums, int[][] queries) {
 
-        Trie trie = new Trie();
-
-        List<String> list = new ArrayList<>();
-
         Arrays.sort(nums);
 
-        List<int[]> offlinequeries = new ArrayList<>();
+        List<int[]> offlineQueries = new ArrayList<>();
+
         for (int i = 0; i < queries.length; i++) {
-            int arr[] = queries[i];
-            offlinequeries.add(new int[] { arr[0], arr[1], i });
+            offlineQueries.add(
+                new int[]{queries[i][0], queries[i][1], i}
+            );
         }
 
-        int[] ans = new int[queries.length];
+        offlineQueries.sort((a, b) ->
+            Integer.compare(a[1], b[1])
+        );
 
-        Collections.sort(offlinequeries, (a, b) -> Integer.compare(a[1], b[1]));
+        Trie trie = new Trie();
+        int[] ans = new int[queries.length];
 
         int idx = 0;
 
-        for (int[] arr : offlinequeries) {
-            int x = arr[0], m = arr[1], ansIdx = arr[2];
-            boolean hasVal = false;
+        for (int[] q : offlineQueries) {
+
+            int x = q[0];
+            int m = q[1];
+            int ansIdx = q[2];
+
             while (idx < nums.length && nums[idx] <= m) {
-
-                String s = Integer.toBinaryString(nums[idx]);
-                s = pad(s);
-
-                trie.insert(s);
+                trie.insert(nums[idx]);
                 idx++;
             }
 
-            ans[ansIdx] = (idx == 0)
-                    ? -1
-                    : trie.max(pad(Integer.toBinaryString(x)));
+            ans[ansIdx] =
+                (idx == 0) ? -1 : trie.maxXor(x);
         }
 
         return ans;
