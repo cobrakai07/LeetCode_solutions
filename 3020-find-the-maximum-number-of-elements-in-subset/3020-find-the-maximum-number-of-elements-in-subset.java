cc533d5b31f861eca8b62b4c2cf33336f191a2c3
Deepak Kumar <76612112+cobrakai07@users.noreply.github.com>
2026-06-27 16:50:56 +0530

    Time: 56 ms (61.63%), Space: 65 MB (77.91%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
@@ -7,23 +7,20 @@ class Solution {
             if(i==1)one++;
             map.put(i * 1L, map.getOrDefault(i*1L, 0) + 1);
         }
-        // System.out.println(map);
+
         one = one%2==0 && one!=0 ?one-1: one;
         int max = Math.max(one,1);
 
         for (long key : map.keySet()) {
             if (key == 1)
                 continue;
-            ///////////
-            boolean flag = true;
-            boolean addOne = false;
+
             int curr = 0;
             while (map.get(key) != null) {
-                // System.out.println(key + " --X," + curr);
+         
                 long sq = 1L * key * key;
 
                 if(map.get(key)<2){
-                    // System.out.println("--cree-");
                     curr++;
                     break ;
                 }
@@ -34,11 +31,8 @@ class Solution {
                     curr++;
                 }
                 
-                key = sq;
-                // System.out.println(key + " --XX," + curr+"---");
-                
+                key = sq;            
             }
-            // System.out.println(curr + "--,,-");
             
             max = Math.max(max, curr);
         }
